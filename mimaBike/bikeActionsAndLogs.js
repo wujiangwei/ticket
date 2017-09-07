21887e5c93fcae203b008f609c5c18a8622d738e
wujiangwei55 <wujiangwei55@gmail.com>
2017-09-07 19:29:04 +0800

    no message

--- mimaBike/bikeActionsAndLogs.js
+++ mimaBike/bikeActionsAndLogs.js
@@ -191,7 +191,7 @@ function structLogContent(leanContentObject) {
             try {
                 authContentObject = JSON.parse(authContentStr);
                 leanContentObject.set('bikeID', authContentObject.bikeID);
-                setBikeMapWithRedis(leanContentObject.get('SN'), authContentObject.bikeID);
+                // setBikeMapWithRedis(leanContentObject.get('SN'), authContentObject.bikeID);
             }catch(err) {
                 console.log('auth with no bikeId ', err.message);
                 console.log(contentStr);
@@ -428,6 +428,6 @@ function deleteOldDateLogs(maxTime, queryDateLess) {
 
 // deleteOldDateLogs(20);
 
-// dealOldDateToStruct();
+dealOldDateToStruct();
 
 module.exports = router
