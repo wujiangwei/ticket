aa0c3c7be57b90fbde87705c6c82a2980044a3e4
吴才龙 <hengxingyema@163.com>
2017-09-21 04:43:59 +0800

    发送报警短信有问题，暂时去除看下

--- mimaBike/bikeActionsAndLogs.js
+++ mimaBike/bikeActionsAndLogs.js
@@ -506,75 +506,138 @@ function alarmBike(sn, satellite, alarmType, leanContentObject) {
                                     bikeId = sn;
                                 }
 
-                                httpUtil.httpGetBicycleState(bikeId,function (bicycleState) {
-                                    if (bicycleState == true){
-                                        return
+                                var areaData = getResponseBody.data;
+                                var ownerData = areaData.PartnerinfoModel;
+                                var operateDatas = areaData.PerationuserModel;
+
+
+                                var phoneList = [];
+                                for(var i = 0; i < operateDatas.length; i++){
+                                    var perationUser = operateDatas[i];
+                                    if(perationUser.NeedWaring == 1){
+                                        phoneList.push(perationUser.UserPhone)
                                     }
-                                    else {
-                                        var areaData = getResponseBody.data;
-                                        var ownerData = areaData.PartnerinfoModel;
-                                        var operateDatas = areaData.PerationuserModel;
+                                }
+                                //老总放到最后提示，无视他是不是接受短信
+                                for(var i = 0; i < operateDatas.length; i++){
+                                    var perationUser = operateDatas[i];
+                                    if(perationUser.UserRealName.indexOf('总') != -1 && phoneList.indexOf(perationUser.UserPhone) == -1){
+                                        phoneList.push(perationUser.UserPhone)
+                                    }
+                                }
+                                //其次是负责人
+                                if(ownerData.PartnerCellPhone != undefined && phoneList.indexOf(ownerData.PartnerCellPhone) == -1){
+                                    phoneList.push(ownerData.PartnerCellPhone);
+                                }
+                                //最后是不接受短信的人
+                                for (var i = 0; i < operateDatas.length; i++) {
+                                    var perationUser = operateDatas[i];
+                                    if (perationUser.NeedWaring != 1 && phoneList.indexOf(perationUser.UserPhone) == -1) {
+                                        phoneList.push(perationUser.UserPhone)
+                                    }
+                                }
 
-
-                                        var phoneList = [];
-                                        for(var i = 0; i < operateDatas.length; i++){
-                                            var perationUser = operateDatas[i];
-                                            if(perationUser.NeedWaring == 1){
-                                                phoneList.push(perationUser.UserPhone)
-                                            }
-                                        }
-                                        //老总放到最后提示，无视他是不是接受短信
-                                        for(var i = 0; i < operateDatas.length; i++){
-                                            var perationUser = operateDatas[i];
-                                            if(perationUser.UserRealName.indexOf('总') != -1 && phoneList.indexOf(perationUser.UserPhone) == -1){
-                                                phoneList.push(perationUser.UserPhone)
-                                            }
-                                        }
-                                        //其次是负责人
-                                        if(ownerData.PartnerCellPhone != undefined && phoneList.indexOf(ownerData.PartnerCellPhone) == -1){
-                                            phoneList.push(ownerData.PartnerCellPhone);
-                                        }
-                                        //最后是不接受短信的人
-                                        for (var i = 0; i < operateDatas.length; i++) {
-                                            var perationUser = operateDatas[i];
-                                            if (perationUser.NeedWaring != 1 && phoneList.indexOf(perationUser.UserPhone) == -1) {
-                                                phoneList.push(perationUser.UserPhone)
-                                            }
-                                        }
-
-                                        //递归
-                                        function alarmToPhone() {
-                                            // return;
-                                            var sendSmsData = {
-                                                mobilePhoneNumber: phoneList[sendPhoneIndex],
-                                                template: 'bikeAlarm',
-                                                bikeNumber: bikeId,
-                                                alarmTime: process.env['illegalityMovePoliceMin'],
-                                                touches: illegalTouch,
-                                                illegalityMove: illegalMove
-                                            };
-                                            alarmSms.sendAlarmSms(sendSmsData, function (Ret) {
-                                                sendPhoneIndex++;
-                                                if(Ret == 0 && sendPhoneIndex < phoneList.length){
-                                                    //发送失败，且有人在，继续发送
-                                                    alarmToPhone();
-                                                }else {
-                                                    //报警成功，删掉这个key，reset
-                                                    redisUtil.redisClient.del(alarmRedisKey, function (err, reply) {
-                                                        if(err != null){
-                                                            console.error('alarmBike del in redis error, ', err.message);
-                                                            return;
-                                                        }
-                                                    });
+                                //递归
+                                function alarmToPhone() {
+                                    // return;
+                                    var sendSmsData = {
+                                        mobilePhoneNumber: phoneList[sendPhoneIndex],
+                                        template: 'bikeAlarm',
+                                        bikeNumber: bikeId,
+                                        alarmTime: process.env['illegalityMovePoliceMin'],
+                                        touches: illegalTouch,
+                                        illegalityMove: illegalMove
+                                    };
+                                    alarmSms.sendAlarmSms(sendSmsData, function (Ret) {
+                                        sendPhoneIndex++;
+                                        if(Ret == 0 && sendPhoneIndex < phoneList.length){
+                                            //发送失败，且有人在，继续发送
+                                            alarmToPhone();
+                                        }else {
+                                            //报警成功，删掉这个key，reset
+                                            redisUtil.redisClient.del(alarmRedisKey, function (err, reply) {
+                                                if(err != null){
+                                                    console.error('alarmBike del in redis error, ', err.message);
+                                                    return;
                                                 }
-                                            })
+                                            });
                                         }
+                                    })
+                                }
 
-                                        var sendPhoneIndex = 0;
-                                        //开始根据发送短信人的优先级发送短信，先接受报警人，其次老板，然后是不接受短信的人
-                                        alarmToPhone(phoneList[sendPhoneIndex]);
-                                    }
-                                })
+                                var sendPhoneIndex = 0;
+                                //开始根据发送短信人的优先级发送短信，先接受报警人，其次老板，然后是不接受短信的人
+                                alarmToPhone(phoneList[sendPhoneIndex]);
+
+                                // httpUtil.httpGetBicycleState(bikeId,function (bicycleState) {
+                                //     if (bicycleState == true){
+                                //         return
+                                //     }
+                                //     else {
+                                //         var areaData = getResponseBody.data;
+                                //         var ownerData = areaData.PartnerinfoModel;
+                                //         var operateDatas = areaData.PerationuserModel;
+                                //
+                                //
+                                //         var phoneList = [];
+                                //         for(var i = 0; i < operateDatas.length; i++){
+                                //             var perationUser = operateDatas[i];
+                                //             if(perationUser.NeedWaring == 1){
+                                //                 phoneList.push(perationUser.UserPhone)
+                                //             }
+                                //         }
+                                //         //老总放到最后提示，无视他是不是接受短信
+                                //         for(var i = 0; i < operateDatas.length; i++){
+                                //             var perationUser = operateDatas[i];
+                                //             if(perationUser.UserRealName.indexOf('总') != -1 && phoneList.indexOf(perationUser.UserPhone) == -1){
+                                //                 phoneList.push(perationUser.UserPhone)
+                                //             }
+                                //         }
+                                //         //其次是负责人
+                                //         if(ownerData.PartnerCellPhone != undefined && phoneList.indexOf(ownerData.PartnerCellPhone) == -1){
+                                //             phoneList.push(ownerData.PartnerCellPhone);
+                                //         }
+                                //         //最后是不接受短信的人
+                                //         for (var i = 0; i < operateDatas.length; i++) {
+                                //             var perationUser = operateDatas[i];
+                                //             if (perationUser.NeedWaring != 1 && phoneList.indexOf(perationUser.UserPhone) == -1) {
+                                //                 phoneList.push(perationUser.UserPhone)
+                                //             }
+                                //         }
+                                //
+                                //         //递归
+                                //         function alarmToPhone() {
+                                //             // return;
+                                //             var sendSmsData = {
+                                //                 mobilePhoneNumber: phoneList[sendPhoneIndex],
+                                //                 template: 'bikeAlarm',
+                                //                 bikeNumber: bikeId,
+                                //                 alarmTime: process.env['illegalityMovePoliceMin'],
+                                //                 touches: illegalTouch,
+                                //                 illegalityMove: illegalMove
+                                //             };
+                                //             alarmSms.sendAlarmSms(sendSmsData, function (Ret) {
+                                //                 sendPhoneIndex++;
+                                //                 if(Ret == 0 && sendPhoneIndex < phoneList.length){
+                                //                     //发送失败，且有人在，继续发送
+                                //                     alarmToPhone();
+                                //                 }else {
+                                //                     //报警成功，删掉这个key，reset
+                                //                     redisUtil.redisClient.del(alarmRedisKey, function (err, reply) {
+                                //                         if(err != null){
+                                //                             console.error('alarmBike del in redis error, ', err.message);
+                                //                             return;
+                                //                         }
+                                //                     });
+                                //                 }
+                                //             })
+                                //         }
+                                //
+                                //         var sendPhoneIndex = 0;
+                                //         //开始根据发送短信人的优先级发送短信，先接受报警人，其次老板，然后是不接受短信的人
+                                //         alarmToPhone(phoneList[sendPhoneIndex]);
+                                //     }
+                                // })
 
                             })
                         }
