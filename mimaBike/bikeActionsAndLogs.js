87c72415faad24b40a8111181e54913e8c85190e
wujiangwei55 <wujiangwei55@gmail.com>
2017-09-21 21:02:59 +0800

    Fix Bugs

--- mimaBike/bikeActionsAndLogs.js
+++ mimaBike/bikeActionsAndLogs.js
@@ -526,7 +526,7 @@ function alarmBike(sn, satellite, alarmType, leanContentObject) {
                                     }
                                 }
                                 //其次是负责人
-                                if((ownerData.PartnerCellPhone != null || ownerData.PartnerCellPhone != undefined) && phoneList.indexOf(ownerData.PartnerCellPhone) == -1){
+                                if(ownerData != null && (ownerData.PartnerCellPhone != null || ownerData.PartnerCellPhone != undefined) && phoneList.indexOf(ownerData.PartnerCellPhone) == -1){
                                     console.log('看看值：' + ownerData.PartnerCellPhone)
                                     phoneList.push(ownerData.PartnerCellPhone);
                                 }
