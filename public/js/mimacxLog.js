ccf32643ae90e0f3022f8b9b2ae309a9ea694fac
吴才龙 <hengxingyema@163.com>
2017-09-23 22:01:28 +0800

    fix 日志网站界面

--- public/js/mimacxLog.js
+++ public/js/mimacxLog.js
@@ -385,7 +385,14 @@ app.controller('mimacxLogCtrl', function($scope, $http, $location) {
 
                         //argument 设置
                         if(serviceData.Content.argument != undefined){
-                            serviceData.cmdSource = serviceData.cmdSource + "Argument: "// + JSON.stringify(serviceData.Content.argument);
+                            var string = JSON.stringify(serviceData.Content.argument)
+                            if (string.length > 5){
+                                serviceData.cmdSource = serviceData.cmdSource;
+                            }
+                            else {
+                                serviceData.cmdSource = serviceData.cmdSource + "Argument: " + JSON.stringify(serviceData.Content.argument);
+                            }
+
                         }else if(serviceData.Content.result != undefined){
                             serviceData.cmdSource = "响应结果Result: " + (serviceData.Content.result == 0 ? '成功' : ('失败' + serviceData.Content.result));
                             if(serviceData.Content.result != 0){
