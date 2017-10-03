152a8c1bfcfd935b3b31c3b2373e6d3ee9fcd135
wujiangwei55 <wujiangwei55@gmail.com>
2017-10-03 09:08:02 +0800

    订单

--- public/js/allBikes.js
+++ public/js/allBikes.js
@@ -269,7 +269,6 @@ app.controller('allBikesCtrl', function($scope, $http, $location) {
             "SN" : eListBikeInfo.ControllerNo
         })
             .then(function(result) {
-                electric
                 eListBikeInfo.lastestOnlineTime = result.data.bikeLatestTime;
                 eListBikeInfo.bikeEState = result.data.bikeEState;
                 if(result.data.bikeLatestTime == undefined){
