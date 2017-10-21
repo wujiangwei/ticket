25ed12f796f3c3e103b7f0d002752b5c3f0f61e1
wujiangwei55 <wujiangwei55@gmail.com>
2017-10-21 21:10:07 +0800

    FFF

--- mimaBike/bikeActionsAndLogs.js
+++ mimaBike/bikeActionsAndLogs.js
@@ -99,7 +99,6 @@ router.post('/', function(req, res) {
     newEBikeLog.set('Remark', LogParam.Remark);
     newEBikeLog.set('SourceType', parseInt(LogParam.SourceType));
     if(LogParam.BicycleNo != undefined && LogParam.BicycleNo.length > 5){
-        console.log('查看车辆号是否正确' + LogParam.BicycleNo)
         newEBikeLog.set('bikeID', LogParam.BicycleNo);
     }
     // 监控socket服务器正常否
