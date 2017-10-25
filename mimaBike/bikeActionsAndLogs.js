4995f67435b08e3d204edbf136e9e0aea8bd13d7
吴才龙 <hengxingyema@163.com>
2017-10-25 11:01:46 +0800

    增加log

--- mimaBike/bikeActionsAndLogs.js
+++ mimaBike/bikeActionsAndLogs.js
@@ -692,6 +692,7 @@ function batteryOff(sn, alarmType) {
                     //not opened battery in 10 min
 
                     if (bikeId != null || bikeId != undefined){
+                        console.log('查看断电redis里状态' + bikeId);
                         httpUtil.httpPost({BicycleNo:bikeId + " | 2 ",Message:"车辆异常断电"})
                         getUserPhoneNumber(sn)
                     }
@@ -941,6 +942,7 @@ function alarmBike(sn, satellite, alarmType, leanContentObject) {
                                 var sendPhoneIndex = 0;
 
                                 if (bikeId != null || bikeId != undefined){
+                                    console.log('查看ID状态' + bikeId);
                                     //开始根据发送短信人的优先级发送短信，先接受报警人，其次老板，然后是不接受短信的人
                                     console.log('---------- bike: ' + bikeId + ' shifting,and start send sms to ' + phoneList[sendPhoneIndex] + '(' + sendPhoneIndex + ')');
                                     alarmToPhone(phoneList[sendPhoneIndex]);
